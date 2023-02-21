b0cbcba7c228faf22ce547ead7566b4bc0bd1422
Lloyd Lobo <lloydlobo@gmail.com>
2023-02-21 11:07:42 +0530

    fix: resolve indentation error after vi edit

--- db.py
+++ db.py
@@ -27,20 +27,28 @@ class DBHelper:
     def setup(self):
         with self.conn:
             c = self.conn.cursor()
-            c.execute(
-                """
-CREATE TABLE IF NOT EXISTS expenses (
-    id INTEGER, 
-    user_id INTEGER, 
-    amount REAL, 
-    category TEXT, 
-    description TEXT, 
-    date TEXT 
-    )"""
-            )
+            stmt = """
+            CREATE TABLE IF NOT EXISTS expenses (
+                id INTEGER, 
+                user_id INTEGER, 
+                amount REAL, 
+                category TEXT, 
+                description TEXT, 
+                date TEXT
+                )
+            """
+            c.execute(stmt)
         pass
 
     def add_expense(self, user_id, amount, category, description, date):
         with self.conn:
-            pass
+            c = self.conn.cursor()
+            stmt = """
+            INSERT INTO expenses ( user_id, amount, category, description, date) 
+                VALUES (?, ?, ?, ?, datetime('now', 'localtime'))
+            """
+            c.execute(
+                stmt,
+                (user_id, amount, category, description, date),
+            )
         pass
